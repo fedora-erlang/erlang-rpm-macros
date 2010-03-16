118081385d30ae048813e8faf3dd82a8e8c1b7c5
Peter Lemenkov <lemenkov@gmail.com>
2010-03-16 16:24:42 +0300

    Since now erlang-find-requires.sh requires "-b  --" switch
    Signed-off-by: Peter Lemenkov <lemenkov@gmail.com>

--- erlang-find-requires.sh
+++ erlang-find-requires.sh
@@ -3,10 +3,17 @@
 # This script reads filenames from STDIN and outputs any relevant requires
 # information that needs to be included in the package.
 
-filelist=`sed "s/['\"]/\\\&/g"`
+BUILDDIR=
 
-# Use ordinary find-requires first.
-echo $filelist | tr [:blank:] '\n' | /usr/lib/rpm/find-requires
+while true; do
+    case "$1" in
+	-b) BUILDDIR="$2"; shift 2;;
+	--) shift; break;;
+	*) echo "$0: option error at $1"; exit 1;;
+    esac
+done
+
+filelist=`sed "s/['\"]/\\\&/g"`
 
 # Get the list of *.app files
 appfiles=$(echo $filelist | tr [:blank:] '\n' | grep '/ebin/' | grep '\.app$')
@@ -18,13 +25,14 @@ for f in $appfiles; do
 	done
 done
 
-# TODO
 # Get include_lib directives
-erlfiles=$(echo $filelist | tr [:blank:] '\n' | grep '\.[eh]rl$')
-for f in $erlfiles ; do
-	apps=`cat $f | tr -d [:blank:] | grep '^\-include_lib' | cut -d \" -f 2|cut -d \/ -f 1`
-	for a in $apps; do
-		echo "erlang($a)"
-	done
-done | sort | uniq
-
+if [ -n "$BUILDDIR" ]
+then
+	erlfiles=$(echo $filelist | tr [:blank:] '\n' | grep '\.[eh]rl$')
+	for f in `find $BUILDDIR -type f -name '*.[eh]rl'` ; do
+		apps=`cat $f | tr -d [:blank:] | grep '^\-include_lib' | cut -d \" -f 2|cut -d \/ -f 1`
+		for a in $apps; do
+			echo "erlang($a)"
+		done
+	done | sort | uniq
+fi
