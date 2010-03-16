7e28dacab9b30eae9fdbc2ef792b4e88daee895a
Peter Lemenkov <lemenkov@gmail.com>
2010-03-16 18:49:11 +0300

    Process *.app file for provides. Do not add to provides bogus ones (without versioning).
    Signed-off-by: Peter Lemenkov <lemenkov@gmail.com>

--- erlang-find-provides.sh
+++ erlang-find-provides.sh
@@ -7,10 +7,22 @@ filelist=`sed "s/['\"]/\\\&/g"`
 
 /usr/lib/rpm/rpmdeps --provides $filelist
 
-basedirs=$(echo $filelist | tr [:blank:] '\n' | awk -F '/erlang/lib/' '{print $2}'|cut -d '/' -f 1 | uniq )
+# Get the list of *.app files
+appfiles=$(echo $filelist | tr [:blank:] '\n' | grep '/ebin/' | grep '\.app$')
 
+for f in $appfiles; do
+	app=`cat $f | tr -d '\n' | tr -d [:blank:] | awk -F '{application,' '{print $2}'|cut -d , -f 1`
+	ver=`cat $f | tr -d '\n' | tr -d [:blank:] | awk -F '{vsn,"' '{print $2}'|cut -d \" -f 1`
+	echo "erlang($app) = $ver"
+done
+
+# Create list of directories and try guessing by directory name
+basedirs=$(echo $filelist | tr [:blank:] '\n' | awk -F '/erlang/lib/' '{print $2}'|cut -d '/' -f 1 | uniq )
 for bd in $basedirs; do
-    basename=`echo $bd | cut -d '-' -f 1`
-    basever=`echo $bd | cut -d '-' -f 2`
-    echo "erlang($basename) = $basever"
+	basename=`echo $bd | cut -d '-' -f 1`
+	basever=`echo $bd | cut -d '-' -f 2`
+	if [ -n "$basever" ]
+	then
+		echo "erlang($basename) = $basever"
+	fi
 done
