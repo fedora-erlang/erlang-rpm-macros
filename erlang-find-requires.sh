97244fb258784be0020fedfcee01cd4de13e51d0
Peter Lemenkov <lemenkov@gmail.com>
2010-04-07 15:48:52 +0400

    Do not process %{_builddir}/%{buildsubdir}
    Signed-off-by: Peter Lemenkov <lemenkov@gmail.com>

--- erlang-find-requires.sh
+++ erlang-find-requires.sh
@@ -3,16 +3,6 @@
 # This script reads filenames from STDIN and outputs any relevant requires
 # information that needs to be included in the package.
 
-BUILDDIR=
-
-while true; do
-    case "$1" in
-	-b) BUILDDIR="$2"; shift 2;;
-	--) shift; break;;
-	*) echo "$0: option error at $1"; exit 1;;
-    esac
-done
-
 filelist=`sed "s/['\"]/\\\&/g"`
 
 /usr/lib/rpm/rpmdeps --requires $filelist
@@ -26,18 +16,6 @@ for f in $appfiles; do
 		echo "erlang($a)"
 	done
 done
-
-# Get include_lib directives
-if [ -n "$BUILDDIR" ]
-then
-	erlfiles=$(echo $filelist | tr [:blank:] '\n' | grep '\.[eh]rl$')
-	for f in `find $BUILDDIR -type f -name '*.[eh]rl'` ; do
-		apps=`cat $f | tr -d [:blank:] | grep -o -E '^\-include_lib\(\".*\"\)' | sed -e "s,.*(\",,g;s,\/.*,,g"`
-		for a in $apps; do
-			echo "erlang($a)"
-		done
-	done | sort | uniq
-fi
 
 # Get the list of *.beam files
 beamfiles=$(echo $filelist | tr [:blank:] '\n' | grep -o -E '.*/ebin/.*\.beam$')
