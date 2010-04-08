7f069732dce7598ac1795b54dad62768936f23d2
Peter Lemenkov <lemenkov@gmail.com>
2010-04-08 19:03:52 +0400

    Special case - BIFs.
    Signed-off-by: Peter Lemenkov <lemenkov@gmail.com>

--- erlang-find-provides.sh
+++ erlang-find-provides.sh
@@ -3,6 +3,18 @@
 # This script reads filenames from STDIN and outputs any relevant provides
 # information that needs to be included in the package.
 
+BUILDDIR=
+NAME=
+
+while true; do
+	case "$1" in
+		-b) BUILDDIR="$2"; shift 2;;
+		-n) NAME="$2"; shift 2;;
+		--) shift; break;;
+		*) echo "$0: option error at $1"; exit 1;;
+	esac
+done
+
 filelist=`sed "s/['\"]/\\\&/g"`
 
 /usr/lib/rpm/rpmdeps --provides $filelist
@@ -30,4 +42,15 @@ done
 # Get the list of *.beam files
 beamfiles=$(echo $filelist | tr [:blank:] '\n' | grep -o -E '.*/ebin/.*\.beam$')
 /usr/lib/rpm/erlang-find-provides.escript $beamfiles | sed s,\',,g
+
+# BIFs from erts - this module is very specific
+if [ "$NAME" == "erlang-erts" ] ;
+then
+	cat $BUILDDIR/erts/emulator/*/erl_bif_list.h 2>/dev/null |\
+		grep -v am__AtomAlias |\
+		grep -o -E 'am_.*\,am_.*\,.\,' |\
+		sed s,am_,,g |\
+		sed -e "s,Plus,+,g;s,Minus,-,g;s,Neqeq,=\/=,g;s,Neq,\/=,g;s,Div,\/,g;s,Eqeq,=\:=,g;s,Eq,==,g;s,Ge,>=,g;s,Gt,>,g;s,Le,=<,g;s,Lt,<,g;s,Times,*,g;s,subtract,--,g;s,append\,,++\,,g" \
+		awk -F \, '{print "erlang(" $1 ":" $2 "/" $3 ")" }'
+fi
 
