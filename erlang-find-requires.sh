3e3d3a881e1f407bca47212c3614a16f4472053a
Peter Lemenkov <lemenkov@gmail.com>
2011-12-01 22:09:51 +0400

    Don't care about *.app files anymore
    Signed-off-by: Peter Lemenkov <lemenkov@gmail.com>

--- erlang-find-requires.sh
+++ erlang-find-requires.sh
@@ -27,16 +27,6 @@ filelist=`sed "s/['\"]/\\\&/g"`
 
 /usr/lib/rpm/rpmdeps --requires $filelist
 
-# Get the list of *.app files
-appfiles=$(echo $filelist | tr [:blank:] '\n' | grep -o -E '.*/ebin/.*\.app$')
-
-for f in $appfiles; do
-	apps=`cat $f | tr -d [:space:] | grep -o -E '\{applications,\[.*[a-zA-Z0-9_]\]\}' | sed -e "s,.*\[,,g;s,\].*,,g;s.,. .g"`
-	for a in $apps; do
-		echo "erlang($a)"
-	done
-done
-
 # Get the list of *.beam files
 beamfiles=$(echo $filelist | tr [:blank:] '\n' | grep -o -E '.*/ebin/.*\.beam$')
 /usr/lib/rpm/erlang-find-requires.escript $beamfiles | sort | uniq
