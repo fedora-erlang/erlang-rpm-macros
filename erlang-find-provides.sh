8a7456fabae6ace622d7405d1f1ec57c9f31339a
Peter Lemenkov <lemenkov@gmail.com>
2016-02-27 22:00:30 +0300

    Use less command calls
    Signed-off-by: Peter Lemenkov <lemenkov@gmail.com>

--- erlang-find-provides.sh
+++ erlang-find-provides.sh
@@ -51,12 +51,12 @@ for f in $appfiles; do
 			sed -e "s,Plus,+,g;s,Minus,-,g;s,Neqeq,=\/=,g;s,Neq,\/=,g;s,Div,\/,g;s,Eqeq,=\:=,g;s,Eq,==,g;s,Ge,>=,g;s,Gt,>,g;s,Le,=<,g;s,Lt,<,g;s,Times,*,g;s,subtract,--,g;s,append\,,++\,,g" |\
 			awk -F \, '{print "erlang(" $1 ":" $2 "/" $3 ")" }'
 
-		ERL_DRV_MAJOR=`grep "^#define\s*ERL_DRV_EXTENDED_MAJOR_VERSION\s*[0-9]$" $BUILDDIR/erts/emulator/beam/erl_driver.h | cut -f 2`
-		ERL_DRV_MINOR=`grep "^#define\s*ERL_DRV_EXTENDED_MINOR_VERSION\s*[0-9]$" $BUILDDIR/erts/emulator/beam/erl_driver.h | cut -f 2`
+		ERL_DRV_MAJOR=`grep -o -P "(?<=^#define\sERL_DRV_EXTENDED_MAJOR_VERSION\s)[0-9]+(?=$)" $BUILDDIR/erts/emulator/beam/erl_driver.h`
+		ERL_DRV_MINOR=`grep -o -P "(?<=^#define\sERL_DRV_EXTENDED_MINOR_VERSION\s)[0-9]+(?=$)" $BUILDDIR/erts/emulator/beam/erl_driver.h`
 		echo "erlang(erl_drv_version) = $ERL_DRV_MAJOR.$ERL_DRV_MINOR"
 
-		ERL_NIF_MAJOR=`grep "^#define\s*ERL_NIF_MAJOR_VERSION\s*[0-9]$" $BUILDDIR/erts/emulator/beam/erl_nif.h | cut -d " " -f 3`
-		ERL_NIF_MINOR=`grep "^#define\s*ERL_NIF_MINOR_VERSION\s*[0-9]$" $BUILDDIR/erts/emulator/beam/erl_nif.h | cut -d " " -f 3`
+		ERL_NIF_MAJOR=`grep -o -P "(?<=^#define\sERL_NIF_MAJOR_VERSION\s)[0-9]+(?=$)" $BUILDDIR/erts/emulator/beam/erl_nif.h`
+		ERL_NIF_MINOR=`grep -o -P "(?<=^#define\sERL_NIF_MINOR_VERSION\s)[0-9]+(?=$)" $BUILDDIR/erts/emulator/beam/erl_nif.h`
 		echo "erlang(erl_nif_version) = $ERL_NIF_MAJOR.$ERL_NIF_MINOR"
 	fi
 done
